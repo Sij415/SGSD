93ad605e4bfce9e79da16babf966229eafdde24a
Sean Hernandez <Seanirvin9@gmail.com>
2025-01-15 16:22:29 +0800

    Update index.php

--- Login/index.php
+++ Login/index.php
@@ -1,11 +1,7 @@
 <?php
-// Enable error reporting
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 // Include the database connection
-include('dbconnect.php');
+include('../dbconnect.php');
 session_start();
 
 // Handle login
