36f6aee18297fd2db8802f1a133d15e5c01555cc
Ranz Guiab <RanzGuiab@users.noreply.github.com>
2025-01-15 21:30:49 +0800

    updated signup and fixes

--- SignUp/index.php
+++ SignUp/index.php
@@ -30,6 +30,7 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -41,35 +42,48 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
 </head>
+
 <body>
-    <div class="login-container">
-        <h1 class="main-heading">Create Account</h1>
-        <p class="sub-heading">Welcome to SGSD! Please enter your details below to Register.</p>
-
-
-    <form action="" class="form-group" method="POST">
-        <label for="first_name">First Name:</label>
-        <input type="text" id="first_name" name="first_name" required><br><br>
-        
-        <label for="last_name">Last Name:</label>
-        <input type="text" id="last_name" name="last_name" required><br><br>
-        
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required><br><br>
-        
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required><br><br>
-
-        <label for="role">Role:</label>
-        <select id="role" name="role" required>
-            <option value="driver">Driver</option>
-            <option value="staff">Staff</option>
-            <option value="admin">Admin</option>
-        </select><br><br>
+<header class="main-header">
+    <nav class="main-nav">
+        <a href="../" class="sgsd-redirect">San Gabriel Softdrinks Delivery</a>
+    </nav>
+</header>
+<div class="login-container">
+    <h1 class="main-heading">Create Account</h1>
+    <hr>
+    <form action="" method="POST" class="form-group-signup">
+        <div class="form-group-signup-name">
+            <div class="form-field-signup">
+                <label for="first_name">First Name:</label>
+                <input type="text" id="first_name" name="first_name" required>
+            </div>
+            <div class="form-field-signup">
+                <label for="last_name">Last Name:</label>
+                <input type="text" id="last_name" name="last_name" required>
+            </div>
+        </div>
+        <div class="form-field-signup">
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+        </div>
+        <div class="form-field-signup">
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+        </div>
+        <div class="form-field-signup">
+            <label for="role">Role:</label>
+            <select id="role" name="role" required>
+                <option value="driver">Driver</option>
+                <option value="staff">Staff</option>
+                <option value="admin">Admin</option>
+            </select>
+        </div>
         <div class="button-group">
-            <input class="login-btn" type="submit" value="Signup">
+            <input class="signup-cont-btn" type="submit" value="Signup">
         </div>
     </form>
-    </div>
+</div>
+
 </body>
 </html>
