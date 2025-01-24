8a25f2d1a778ad4fb6d68fc2e5d000a9c05e0b79
jsmadrid23 <joshuamadrid1423@gmail.com>
2025-01-24 14:59:26 +0800

    updated dbconnect

--- dbconnect.php
+++ dbconnect.php
@@ -8,10 +8,9 @@ $database = "SGSD";
 // Create a connection
 $conn = new mysqli($host, $username, $password, $database);
 
-// Check connection
+// // Check connection
 // if ($conn->connect_error) {
 //     die("Connection failed: " . $conn->connect_error);
 // }
 // echo "Connected successfully to the database.";
-return $conn;
 ?>
