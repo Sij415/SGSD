0e87468b69d1628c0a52853e4c2dd60fb680d2b9
jsmadrid23 <joshuamadrid1423@gmail.com>
2025-02-17 12:58:23 +0800

    changes for composer

--- test/generate-pdf.php
+++ test/generate-pdf.php
@@ -1,58 +1,71 @@
 <?php
 
-
-require  "../vendor/autoload.php";
+require "../vendor/autoload.php";
+require "../dbconnect.php"; // Database connection
 
 use Dompdf\Dompdf;
 use Dompdf\Options;
 
-$name = $_POST["name"];
-$quantity = $_POST["quantity"];
-
-//$html = '<h1 style="color: green">Example</h1>';
-//$html .= "Hello <em>$name</em>";
-//$html .= '<img src="example.png">';
-//$html .= "Quantity: $quantity";
-
-/**
- * Set the Dompdf options
- */
+// Set Dompdf options
 $options = new Options;
 $options->setChroot(__DIR__);
 $options->setIsRemoteEnabled(true);
 
+// Initialize Dompdf
 $dompdf = new Dompdf($options);
 
-/**
- * Set the paper size and orientation
- */
+// Fetch transaction data from the database
+$query = "SELECT t.Transaction_ID, t.Date, t.Time, 
+                 c.First_Name AS Customer_First, c.Last_Name AS Customer_Last,
+                 p.Product_Name, o.Amount, u.First_Name AS Staff_First, u.Last_Name AS Staff_Last
+          FROM Transactions t
+          INNER JOIN Orders o ON t.Order_ID = o.Order_ID
+          INNER JOIN Customers c ON t.Customer_ID = c.Customer_ID
+          INNER JOIN Products p ON o.Product_ID = p.Product_ID
+          INNER JOIN Users u ON o.User_ID = u.User_ID
+          ORDER BY t.Date DESC, t.Time DESC";
+
+$result = $conn->query($query);
+
+$html = '<h2 style="text-align: center;">Transaction Records</h2>';
+$html .= '<table border="1" width="100%" cellpadding="5" cellspacing="0">
+            <tr>
+                <th>Transaction ID</th>
+                <th>Date</th>
+                <th>Time</th>
+                <th>Customer</th>
+                <th>Product</th>
+                <th>Amount</th>
+                <th>Processed By</th>
+            </tr>';
+
+while ($row = $result->fetch_assoc()) {
+    $html .= "<tr>
+                <td>{$row['Transaction_ID']}</td>
+                <td>{$row['Date']}</td>
+                <td>{$row['Time']}</td>
+                <td>{$row['Customer_First']} {$row['Customer_Last']}</td>
+                <td>{$row['Product_Name']}</td>
+                <td>{$row['Amount']}</td>
+                <td>{$row['Staff_First']} {$row['Staff_Last']}</td>
+              </tr>";
+}
+
+$html .= '</table>';
+
+// Load the HTML content
+$dompdf->loadHtml($html);
+
+// Set paper size and orientation
 $dompdf->setPaper("A4", "landscape");
 
-/**
- * Load the HTML and replace placeholders with values from the form
- */
-$html = file_get_contents("template.html");
-
-$html = str_replace(["{{ name }}", "{{ quantity }}"], [$name, $quantity], $html);
-
-$dompdf->loadHtml($html);
-//$dompdf->loadHtmlFile("template.html");
-
-/**
- * Create the PDF and set attributes
- */
+// Render the PDF
 $dompdf->render();
 
-$dompdf->addInfo("Title", "An Example PDF"); // "add_info" in earlier versions of Dompdf
+// Stream the PDF to browser (inline display)
+$dompdf->stream("transactions.pdf", ["Attachment" => false]);
 
-/**
- * Send the PDF to the browser
- */
-$dompdf->stream("invoice.pdf", ["Attachment" => 0]);
+// Save the PDF file locally (optional)
+file_put_contents("transactions.pdf", $dompdf->output());
 
-/**
- * Save the PDF file locally
- */
-$output = $dompdf->output();
-file_put_contents("file.pdf", $output);
 ?>
