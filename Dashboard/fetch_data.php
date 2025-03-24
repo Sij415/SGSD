bbf8b7956b75f3742cda1e84d602903b4285e3bd
Sean Hernandez <Seanirvin9@gmail.com>
2025-03-24 19:25:59 +0800

    Update fetch_data.php

--- Dashboard/fetch_data.php
+++ Dashboard/fetch_data.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 include '../dbconnect.php';
 
 $period = isset($_GET['period']) ? $_GET['period'] : 'monthly'; // Default to monthly
